0b94c539a8fd72ad2a3326b77a4eb2958ce71e49
NannaWH <nanna_emilie@hotmail.com>
2025-09-17 13:10:49 -0700

    min-max finder created with function to create number list

---
+++ module03/min-max_finder.py
@@ -0,0 +1,13 @@
+# Write a script to find the minimal and maximal
+# This script is designed to find the minimal and maximal of a sequence of numbers
+
+#We ask the user to input the numbers used in the min-max finder
+number_list = input("Type numbers separated by spaces:")
+
+#Output of the script
+print(number_list)
+
+
+
+
+
