be99f917e51295928c23416385dbb403b6a53b51
NannaWH <nanna_emilie@hotmail.com>
2025-09-17 13:20:41 -0700

    min-max finder updated to include if statement for no inputs and print of output

--- module03/min-max_finder.py
+++ module03/min-max_finder.py
@@ -4,8 +4,18 @@
 #We ask the user to input the numbers used in the min-max finder
 number_list = input("Type numbers separated by spaces:")
 
+#In this function we find the maximum and minimum value in the list of numbers
+if number_list == "": 
+    max_value = "None"
+    min_value = "None"
+
 #Output of the script
-print(number_list)
+print("The list of number is:", number_list)
+print("The minimal number is:", min_value)
+print("The maximal number is:", max_value)
+
+
+
 
 
 
