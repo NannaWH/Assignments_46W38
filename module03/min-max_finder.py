78be70187d53551cf45b313c5ad202ea0950bf31
NannaWH <nanna_emilie@hotmail.com>
2025-09-17 13:51:55 -0700

    min-max finalised with sorting the list of numbers and extracting the first and last number in the list

--- module03/min-max_finder.py
+++ module03/min-max_finder.py
@@ -4,10 +4,23 @@
 #We ask the user to input the numbers used in the min-max finder
 number_list = input("Type numbers separated by spaces:")
 
+#We make sure that the input is a list of numbers and not string variables
+number_list = number_list.split()            
+number_list = [int(num) for num in number_list]  
+
+#We sort the list of numbers so it would be easy to extract the highest and lowest number in the list.
+number_list.sort() 
+
+#defining the number of numbers in the list
+items_in_list = len(number_list)
+
 #In this function we find the maximum and minimum value in the list of numbers
 if number_list == "": 
-    max_value = "None"
     min_value = "None"
+    max_value = "None"
+else:  
+    min_value = number_list[0]
+    max_value = number_list[items_in_list-1]
 
 #Output of the script
 print("The list of number is:", number_list)
